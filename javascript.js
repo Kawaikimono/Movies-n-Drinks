f3145f3d82ab0d5d17a9a8c7cb4f2305ba775af5
Aly-gator <119552961+Kawaikimono@users.noreply.github.com>
2023-01-11 14:46:02 -0800

    Fix javascript

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
-// var alcoholType = document.getElementbyId(drinkdropbtn).value
-// var genreSelected = document.getElementbyId(genredropbtn).value
+// var selectAlcoholType = document.getElementbyId(drinkdropbtn)
+// var selectGenreSelected = document.getElementbyId(genredropbtn)
+
+var genre = ['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Family', 'Horror', 'Mystery', 'Romance', 'Thriller']
+var genreId = ['28', '12','35','80','18','10751', '27', '9648','10749','53']
+var alcohol = ['Gin', 'Tequila', 'Whiskey', 'Scotch', 'Vodka', 'Rum' ]
+// need to pull 'strDrink'.value to different api loop to search for cocktails
 
 function getMovieApi() {
     var requestUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=c21251ae5e77e4922c5ef1b09e36611a&language=en-US';
