1fd916e2d179b4ce7ab73a4733380eb3133d0085
Aly-gator <119552961+Kawaikimono@users.noreply.github.com>
2023-01-12 10:38:43 -0800

    syncing with main

--- javascript.js
+++ javascript.js
@@ -7,20 +7,20 @@ var alcohol = ['Gin', 'Tequila', 'Whiskey', 'Scotch', 'Vodka', 'Rum','Brandy' ]
 var drinkId = 0
 // need to pull 'idDrink'.value to different api loop to search for cocktails
 
-// function getMovieApi() {
-//     var requestUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=c21251ae5e77e4922c5ef1b09e36611a&language=en-US';
+function getMovieApi() {
+    var requestUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=c21251ae5e77e4922c5ef1b09e36611a&language=en-US';
 
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         return response.json();
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
         
-//       }) 
-//       .then(function (data) {
-//         console.log(data)
-//       })
-//       };
+      }) 
+      .then(function (data) {
+        console.log(data)
+      })
+      };
 
-// /discover/movie?with_genres=
+// https://api.themoviedb.org/3/discover/movie?api_key=c21251ae5e77e4922c5ef1b09e36611a&language=en-US&with_genres=12
     
 var listOfDrinksUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin'
 // ${alcohol}
@@ -54,4 +54,4 @@ var listOfDrinksUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=
           }
 
         getDrinkApi();
-        // getMovieApi();
+        getMovieApi();
