cd4963729c65ce437687173dd99626fd344ffff6
kiman21 <kim.andrew0121@gmail.com>
2023-01-16 14:32:51 -0800

    updating-printDrink

--- javascript.js
+++ javascript.js
@@ -160,6 +160,9 @@ function getDrinkApi() {
     .then(theDrink);
 }
 
+var getDrinkBtn = document.querySelector(".drinkbtn");
+getDrinkBtn.addEventListener("click", getDrinkApi);
+
 function printDrink(drink) {
   var drinkImageLoc = document.querySelector("#drink-img");
   var drinkNameLoc = document.querySelector("#drink-title");
@@ -168,14 +171,18 @@ function printDrink(drink) {
   drinkImageLoc.setAttribute("src", drink.strDrinkThumb);
   drinkNameLoc.textContent = drink.strDrink;
   drinkRecipeLoc.textContent = drink.strInstructions;
-  for (var i = 1; i < drink.strIngredient; i++) {
-    var ingredient = drink[`strIngredient${i+1}`];
-    var measure = drink[`strMeasure${i+1}`];
+  for (var i = 1; i < 16; i++) {
+    var ingredient = drink[`strIngredient${i}`];
+    var measure = drink[`strMeasure${i}`];
+    if (ingredient == null) {
+      return;
+    }
+    if (measure ==null) {
+      return;
+    }
     var ingredientList = document.createElement("li");
     ingredientList.textContent = measure + " " + ingredient;
     drinkIngrLoc.append(ingredientList);
   }
-}
 
-var getDrinkBtn = document.querySelector(".drinkbtn");
-getDrinkBtn.addEventListener("click",getDrinkApi)
+}
